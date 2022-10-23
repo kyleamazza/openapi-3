71fd1759487383a1be641e097b61592e05f9caf0
Steve Konves <stephen.konves@gmail.com>
2022-10-23 15:11:58 -0700

    chore: clean up shapshot script

--- src/snapshot/create-snapshot.ts
+++ src/snapshot/create-snapshot.ts
@@ -3,46 +3,26 @@ import { join } from 'path';
 import { format } from 'prettier';
 import parser from '..';
 
-const examplePath = join(process.cwd(), 'src', 'snapshot', 'example.oas3.json');
-const exampleSchema = readFileSync(examplePath).toString('utf8');
-
-const petstorePath = join(
-  process.cwd(),
-  'src',
-  'snapshot',
-  'petstore.oas3.json',
-);
-const petstoreSchema = readFileSync(petstorePath).toString('utf8');
-
-const prettierOptions = JSON.parse(
-  readFileSync(join(process.cwd(), '.prettierrc')).toString('utf8'),
-);
-
-const exampleResult = parser(exampleSchema, examplePath);
-const petstoreResult = parser(petstoreSchema, petstorePath);
-
-const example = exampleResult.service;
-const petstore = petstoreResult.service;
-
-for (const violation of exampleResult.violations) console.warn(violation);
-for (const violation of petstoreResult.violations) console.warn(violation);
-
-const exampleSnapshot = format(JSON.stringify(example), {
-  ...prettierOptions,
-  parser: 'json',
-});
-
-const petstoreSnapshot = format(JSON.stringify(petstore), {
-  ...prettierOptions,
-  parser: 'json',
-});
-
-writeFileSync(
+createSnapshot(
+  join(process.cwd(), 'src', 'snapshot', 'example.oas3.json'),
   join(process.cwd(), 'src', 'snapshot', 'snapshot.json'),
-  exampleSnapshot,
 );
 
-writeFileSync(
+createSnapshot(
+  join(process.cwd(), 'src', 'snapshot', 'petstore.oas3.json'),
   join(process.cwd(), 'src', 'snapshot', 'petstore.json'),
-  petstoreSnapshot,
 );
+
+function createSnapshot(source: string, destination: string): void {
+  const schema = readFileSync(source).toString('utf8');
+  const prettierOptions = JSON.parse(
+    readFileSync(join(process.cwd(), '.prettierrc')).toString('utf8'),
+  );
+  const { service, violations } = parser(schema, source);
+  const snapshot = format(JSON.stringify(service), {
+    ...prettierOptions,
+    parser: 'json',
+  });
+  writeFileSync(destination, snapshot);
+  for (const violation of violations) console.warn(violation);
+}
